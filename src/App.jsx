48840e7570f1b3d8d9c2b58321a94c0554a0ab4a
inathan44 <inathan44@yahoo.com>
2023-05-11 16:03:17 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='m-3 text-green-500'>Hello hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
