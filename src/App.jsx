2a2be0c1988a3f0283244ca15c140c8a4c52a306
Alex Bernt <alexbernt100@gmail.com>
2023-05-17 18:12:10 -0500

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import './App.css';
-import HomePage from './HomePage';
-import Navbar from './Navbar';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import HomePage from "./HomePage";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<HomePage />} />
-        <Route exact path='/about' element={<h1>hi</h1>} />
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/about" element={<h1>hi</h1>} />
       </Routes>
+      <Footer />
     </>
   );
 }
