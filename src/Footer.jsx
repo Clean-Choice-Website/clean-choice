b912cadd7a40be7dc2d5a3d76adc2dcabd2ae737
Alex Bernt <alexbernt100@gmail.com>
2023-05-21 15:36:28 -0500

    Create Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,61 +1,64 @@
 import "./App.css";
+import twitter from "./assets/twitter-sign.png";
+import facebook from "./assets/facebook.png";
+import linkedin from "./assets/linkedin.png";
+import cleanChoice from "./assets/CleanChoice.png";
 import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <>
-      <footer className="flex flex-col items-center justify-between bg-slate-100 px-16 py-8 sm:flex-row">
+      <footer className="mx-auto flex max-w-7xl flex-col items-center justify-between px-16 py-8 md:flex-row md:px-6 lg:px-16">
         <Link to="/">
-          <div className="flex items-center gap-2">
+          <div className="md: flex items-center gap-2 pb-4">
             <img
-              src="https://cleanchoicewi.com/wp-content/uploads/2015/07/Med-Final.png"
+              src={cleanChoice}
+              alt="Clean Choice icon"
               className="w-36 sm:w-40 md:w-44"
             />
           </div>
         </Link>
         <div>
-          <ul className="flex flex-col items-center gap-8 sm:flex-row sm:items-start">
+          <ul className="flex flex-col items-center gap-4 md:flex-row md:items-start md:gap-8">
             <Link to="/about">
-              <li className="text-md w-20 font-bold hover:text-green-600 md:w-20 md:text-lg lg:w-24 xl:w-36">
+              <li className="text-md w-20 font-bold hover:text-green-800 md:w-20 lg:w-32 lg:text-lg">
                 About Us
               </li>
             </Link>
-            <div>
-              <li className="text-md w-20 font-bold md:w-20 md:text-lg lg:w-24 xl:w-36">
+            <Link to="/services">
+              <li className="text-md w-20 font-bold hover:text-green-800 md:w-20 lg:w-32 lg:text-lg">
                 Services
               </li>
-              {/* <div>
-                  <ul className="text-xs md:text-sm">
-                    <Link to="/GeneralCleaning">
-                      <li>General Cleaning</li>
-                    </Link>
-                    <Link to="/CommercialCleaning">
-                      <li>Commercial Cleaning</li>
-                    </Link>
-                    <Link to="/FloorCare">
-                      <li>Floor Care {`&`} Tile Re-Waxing</li>
-                    </Link>
-                  </ul>
-                </div> */}
-            </div>
+            </Link>
             <Link to="/reviews">
-              <li className="text-md w-20 font-bold hover:text-green-600 md:w-20 md:text-lg lg:w-24 xl:w-36">
+              <li className="text-md w-20 font-bold hover:text-green-800 md:w-20 lg:w-32 lg:text-lg">
                 Reviews
               </li>
             </Link>
             <div>
               <Link to="/contact">
-                <li className="text-md text-center font-bold hover:text-green-600 sm:text-left md:w-20 md:text-lg lg:w-24 xl:w-36">
+                <li className="text-md text-center font-bold hover:text-green-800 md:w-24 md:text-left lg:w-32 lg:text-lg">
                   Contact Us
                 </li>
               </Link>
               <div>
-                <ul className="flex flex-col items-center text-xs sm:block md:text-sm">
+                <ul className="flex flex-col items-center text-xs md:block md:text-sm">
                   <li>(414) 310-9206</li>
                   <li>michelle@cleanchoicewi.com</li>
                 </ul>
               </div>
             </div>
+            <ul className="flex md:hidden">
+              <li className="px-1">
+                <img src={twitter} alt="Twitter icon" className="w-6" />
+              </li>
+              <li className="px-1">
+                <img src={facebook} alt="Facebook icon" className="w-6" />
+              </li>
+              <li className="px-1">
+                <img src={linkedin} alt="Linkedin icon" className="w-6" />
+              </li>
+            </ul>
           </ul>
         </div>
       </footer>
