08ae999d4bc253472280290133131bc433057466
Alex Bernt <alexbernt100@gmail.com>
2023-06-01 21:47:50 -0500

    Create ContactInfo Component

---
+++ src/components/ContactInfo.jsx
@@ -0,0 +1,24 @@
+import phone from "../assets/phone.svg";
+import email from "../assets/email.svg";
+import pin from "../assets/pin.png";
+
+const ContactInfo = () => {
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-3 bg-slate-200 py-6 text-sm sm:flex-row sm:gap-7 md:gap-16 md:text-base lg:gap-32">
+      <div className="flex items-center gap-2 font-bold">
+        <img src={phone} alt="Phone icon" className="w-6" />
+        <a href="tel:4143109206">(414)310-9206</a>
+      </div>
+      <div className="flex items-center gap-2 font-bold">
+        <img src={email} alt="Email icon" className="w-7" />
+        <p>michelle@cleanchoicewi.com</p>
+      </div>
+      <div className="flex items-center gap-2 font-bold">
+        <img src={pin} alt="Map pin icon" className="w-6" />
+        <p>Southeastern WI</p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
