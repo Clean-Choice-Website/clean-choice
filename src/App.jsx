e2ba14553e3e12328d81d1ab1c6a461fb5a2a1b1
inathan44 <inathan44@yahoo.com>
2023-06-03 23:31:30 -0400

    Added Reviews page to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import "./App.css";
-import { useState } from "react";
-import HomePage from "./Homepage/HomePage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import AboutUs from "./AboutUs";
-import Services from "./Services";
-import Careers from "./Careers";
-import { Routes, Route } from "react-router-dom";
-import ContactModal from "./components/ContactModal";
+import './App.css';
+import { useState } from 'react';
+import HomePage from './Homepage/HomePage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import AboutUs from './AboutUs';
+import Services from './Services';
+import Careers from './Careers';
+import { Routes, Route } from 'react-router-dom';
+import ContactModal from './components/ContactModal';
+import Reviews from './Reviews';
 
 function App() {
   const [showModal, setShowModal] = useState(false);
@@ -24,10 +25,11 @@ function App() {
     <>
       <Navbar openModal={openModal} />
       <Routes>
-        <Route exact path="/" element={<HomePage openModal={openModal} />} />
-        <Route exact path="/about" element={<AboutUs />} />
-        <Route exact path="/services" element={<Services />} />
-        <Route exact path="/careers" element={<Careers />} />
+        <Route exact path='/' element={<HomePage openModal={openModal} />} />
+        <Route exact path='/about' element={<AboutUs />} />
+        <Route exact path='/services' element={<Services />} />
+        <Route exact path='/careers' element={<Careers />} />
+        <Route exact path='/reviews' element={<Reviews />} />
       </Routes>
       <Footer />
       {showModal && <ContactModal closeModal={closeModal} />}
