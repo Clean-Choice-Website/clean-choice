18618d82e72de7b4500ed391535de0dd43a21eff
inathan44 <inathan44@yahoo.com>
2023-06-05 00:55:13 -0400

    Changed max width

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -3,7 +3,7 @@ import defaultUser from '../assets/default-user.png';
 const Review = () => {
   return (
     <div className='mx-auto flex max-w-5xl justify-center'>
-      <div className='mx-4 max-w-[320px] rounded bg-white p-3 shadow-md md:mx-6 md:flex md:w-auto md:max-w-none md:flex-row-reverse md:items-center md:px-7 md:py-6'>
+      <div className='mx-4 max-w-[450px] rounded bg-white p-3 shadow-md md:mx-6 md:flex md:w-auto md:max-w-none md:flex-row-reverse md:items-center md:px-7 md:py-6'>
         <p className='mb-4 text-sm font-light text-gray-700 md:mb-0 md:w-2/3'>
           Lorem ipsum dolor sit amet consect etur, adipisicing elit. Non,
           deleniti consectetur nihil quaerat soluta explicabo enim Lorem ipsum
