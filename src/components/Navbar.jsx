bb87b9209deb0a9fda411fc21741c9bf8b3460ea
inathan44 <inathan44@yahoo.com>
2023-06-08 17:45:09 -0400

    Removed services dropdown, close menu logo click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,26 @@
 /* eslint-disable react/prop-types */
 import '../App.css';
-import downArrow from '../assets/downArrow.svg';
 import menuIcon from '../assets/menuIcon.svg';
 import { Link } from 'react-router-dom';
-import ServicesDropDown from './ServicesDropDown';
 import { useState } from 'react';
 import MobileNavbar from './MobileNavbar';
 import clearchoicevertical from '../assets/cleanchoicevertical.png';
 
 function Navbar({ openModal }) {
-  const [showServices, setShowServices] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
 
   return (
     <header className='sticky top-0 z-20 bg-white drop-shadow-sm'>
-      <nav className='top-0 z-10 flex items-center justify-between px-9 py-6'>
+      <nav className='top-0 z-10 flex items-center justify-between px-3 py-6 sm:px-9 md:px-4'>
         <Link to='/'>
-          <div className='flex items-center gap-2'>
+          <div
+            className='flex items-center gap-2'
+            onClick={() => setShowMenu(false)}
+          >
             <img
               src={clearchoicevertical}
               alt='Clean Choice logo'
-              className='w-60'
+              className='w-48 lg:w-60'
             />
             {/*    */}
           </div>
@@ -37,25 +37,15 @@ function Navbar({ openModal }) {
           <Link to='/about'>
             <li className='list-none font-medium'>About Us</li>
           </Link>
-          <div
-            className='relative flex items-center gap-1'
-            onMouseEnter={() => setShowServices(true)}
-            onMouseLeave={() => setShowServices(false)}
-          >
+          <Link to='/services'>
             <li className='list-none font-medium'>Services</li>
-            <img src={downArrow} alt='Down Arrow' className='w-5' />
-            {showServices && <ServicesDropDown />}
-          </div>
+          </Link>
           <Link to='/reviews'>
             <li className='list-none font-medium'>Reviews</li>
           </Link>
           <Link to='/careers'>
             <li className='list-none font-medium'>Careers</li>
           </Link>
-          <button onClick={openModal}>
-            <li className='list-none font-medium'>Contact Us</li>
-          </button>
-
           <button
             onClick={openModal}
             className='rounded-sm bg-logoGreen  p-2 px-5 font-medium text-white hover:bg-logoGreenHover'
@@ -65,7 +55,9 @@ function Navbar({ openModal }) {
         </div>
       </nav>
       {/* Mobile Nav */}
-      {showMenu && <MobileNavbar setShowMenu={setShowMenu} />}
+      {showMenu && (
+        <MobileNavbar setShowMenu={setShowMenu} openModal={openModal} />
+      )}
     </header>
   );
 }
