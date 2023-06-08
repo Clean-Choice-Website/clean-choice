7ce9a48679a895a00ee315f2fe429b58bb069b75
inathan44 <inathan44@yahoo.com>
2023-06-08 17:44:23 -0400

    Removed services drop, added contact modal

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -1,104 +1,62 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom";
-import downArrow from "../assets/downArrow.svg";
-import phone from "../assets/phone.svg";
-import { useState } from "react";
+import { Link } from 'react-router-dom';
+import phone from '../assets/phone.svg';
 
 const MobileNavbar = ({ setShowMenu, openModal }) => {
-  const [showServices, setShowServices] = useState(false);
-
   return (
-    <div className="absolute z-20 h-[calc(100vh_-_100px)] w-full border bg-black bg-opacity-50 md:hidden">
-      <div className=" z-20 w-full border-t-2 border-gray-400 bg-slate-100">
-        <ul className="mx-4 pb-2 text-xl">
-          <Link to="/about">
+    <div className='absolute z-20 h-[calc(100vh_-_100px)] w-full border bg-black bg-opacity-50 md:hidden'>
+      <div className=' z-20 w-full border-t-2 border-gray-400 bg-slate-100'>
+        <ul className='mx-4 pb-2 text-xl'>
+          <Link to='/about'>
             <li
               onClick={() => setShowMenu(false)}
-              className="border-b border-gray-300 py-4"
+              className='border-b border-gray-300 py-4'
             >
               About Us
             </li>
           </Link>
-          <button
-            onClick={() => setShowServices((prev) => !prev)}
-            className="flex w-full basis-full items-center justify-between border-b border-gray-300 text-left"
-          >
-            <li className="w-full py-4">Services</li>
-            <img
-              src={downArrow}
-              alt="Down Arrow"
-              className={`w-7 ${showServices ? "rotate-180" : ""}`}
-            />
-          </button>
 
-          {showServices && (
-            <div className="">
-              <Link to="/services">
-                <li
-                  onClick={() => setShowMenu(false)}
-                  className="border-b border-gray-300 px-5 py-4 text-lg"
-                >
-                  General Cleaning
-                </li>
-              </Link>
-              <Link to="/services">
-                <li
-                  onClick={() => setShowMenu(false)}
-                  className="border-b border-gray-300 px-5 py-4 text-lg"
-                >
-                  Commercial Cleaning
-                </li>
-              </Link>
-              <Link to="/services">
-                <li
-                  onClick={() => setShowMenu(false)}
-                  className="border-b border-gray-300 px-5 py-4 text-lg"
-                >
-                  Floor Care & Tile Re-Waxing
-                </li>
-              </Link>
-            </div>
-          )}
-          <Link to="/Reviews">
+          <Link to='/services'>
             <li
               onClick={() => setShowMenu(false)}
-              className="border-b border-gray-300 py-4"
+              className='border-b border-gray-300 py-4'
+            >
+              Services
+            </li>
+          </Link>
+
+          <Link to='/Reviews'>
+            <li
+              onClick={() => setShowMenu(false)}
+              className='border-b border-gray-300 py-4'
             >
               Reviews
             </li>
           </Link>
-          <Link to="/careers">
+          <Link to='/careers'>
             <li
               onClick={() => setShowMenu(false)}
-              className="border-b border-gray-300 py-4"
+              className='border-b border-gray-300 py-4'
             >
               Careers
             </li>
           </Link>
-          <Link to="/contact">
-            <li
-              onClick={() => setShowMenu(false)}
-              className="border-b border-gray-300 py-4"
+          {/* <Link> */}
+          <div
+            onClick={() => setShowMenu(false)}
+            className='m-2 flex justify-center'
+          >
+            <button
+              onClick={openModal}
+              className='mb-3 w-full rounded border-2 border-white bg-logoGreen px-8 py-2 text-center text-lg font-bold text-white hover:bg-logoGreenHover'
             >
-              Contact
-            </li>
-          </Link>
-          <Link>
-            <div
-              onClick={() => setShowMenu(false)}
-              className="m-2 flex justify-center"
-            >
-              <button
-                onClick={open}
-                className="mb-3 w-full rounded border-2 border-white bg-logoGreen px-8 py-2 text-center text-lg font-bold text-white hover:bg-logoGreenHover"
-              >
-                Get a Quote
-              </button>
-            </div>
-          </Link>
-          <li className="flex justify-center gap-2 border-b border-gray-300 py-4 text-slate-500">
-            <img src={phone} alt="Phone icon" className="w-6" />
-            <a href="tel:4143109206">(414)310-9206</a>
+              Get a Quote
+            </button>
+          </div>
+          {/* </Link> */}
+          <li className='flex justify-center gap-2 border-b border-gray-300 py-4 text-slate-500'>
+            <img src={phone} alt='Phone icon' className='w-6' />
+            <a href='tel:4143109206'>(414)310-9206</a>
           </li>
         </ul>
       </div>
