c19287402336e94b8a4563fe0450cc85ac25b362
inathan44 <inathan44@yahoo.com>
2023-06-08 17:45:53 -0400

    Scroll to top on page load, button effect

--- src/Homepage/HomePage.jsx
+++ src/Homepage/HomePage.jsx
@@ -1,9 +1,16 @@
-import HomePageHero from "./HomePageHero";
-import HomePageAbout from "./HomePageAbout";
-import HomePageServices from "./HomePageServices";
-import HomePageTestimonials from "./HomePageTestimonials";
+/* eslint-disable react/prop-types */
+import { useEffect } from 'react';
+import { scrollToTop } from '../utils/scrollToTop';
+import HomePageHero from './HomePageHero';
+import HomePageAbout from './HomePageAbout';
+import HomePageServices from './HomePageServices';
+import HomePageTestimonials from './HomePageTestimonials';
 
 const HomePage = ({ openModal }) => {
+  useEffect(() => {
+    scrollToTop();
+  }, []);
+
   return (
     <>
       <HomePageHero openModal={openModal} />
