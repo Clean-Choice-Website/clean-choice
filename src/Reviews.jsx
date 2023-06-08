3298986dfc7d41843a99d81812e73fcba874f70f
inathan44 <inathan44@yahoo.com>
2023-06-08 17:44:44 -0400

    Removed image icon, added review form

--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -1,9 +1,43 @@
-// import Testimonial from './components/Testimonial';
-
+import { useEffect, useState } from 'react';
+// import emailjs from 'emailjs-com';
 import Review from './components/Review';
 import StarIcon from './components/StarIcon';
+import { scrollToTop } from './utils/scrollToTop';
 
 const Reviews = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Replace "YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", and "YOUR_USER_ID" with your actual values
+    // emailjs
+    //   .sendForm(
+    //     'service_ge8matn',
+    //     'template_nh0bluk',
+    //     e.target,
+    //     'bD-Rm5YFO4MwTl9Py'
+    //   )
+    //   .then(() => {
+    //     alert('Your message has been sent successfully!');
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error sending message:', error);
+    //   });
+  };
+
+  useEffect(() => {
+    scrollToTop();
+  }, []);
+
   return (
     <div className=' bg-slate-100 pb-8'>
       <div className='mb-12 flex flex-col  items-center bg-green-700 py-5'>
@@ -32,6 +66,46 @@ const Reviews = () => {
         <Review />
         <Review />
         <Review />
+      </div>
+      <div className='mt-16 text-center'>
+        <h1 className='mb-3 mt-2 text-3xl font-bold'>Leave A Review</h1>
+        {/* <p className='mb-4'>We are here for you! How can we help?</p> */}
+        <form onSubmit={handleSubmit} className='mx-4 space-y-5'>
+          <input
+            type='text'
+            name='name'
+            placeholder='Your Name'
+            value={formData.name}
+            onChange={handleChange}
+            required
+            className='w-full rounded-md border border-gray-300 p-2'
+          />
+          <input
+            type='email'
+            name='email'
+            placeholder='Your Email'
+            value={formData.email}
+            onChange={handleChange}
+            required
+            className='w-full rounded-md border border-gray-300 p-2'
+          />
+          <textarea
+            name='message'
+            placeholder='Your Message'
+            value={formData.message}
+            onChange={handleChange}
+            required
+            className='h-32 w-full rounded-md border border-gray-300 p-2'
+          ></textarea>
+          <div className='flex justify-start'>
+            <button
+              type='submit'
+              className='mr-4 rounded-sm bg-logoGreen px-6 py-2 font-bold text-white hover:bg-logoGreenHover'
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
