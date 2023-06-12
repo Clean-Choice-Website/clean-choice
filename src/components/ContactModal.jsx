1ffb152a0c770476363f9a91cb8780c744a1a743
Chance Hemerda <cshemerda@gmail.com>
2023-06-12 11:39:57 -0700

    added success modal for contact submission

--- src/components/ContactModal.jsx
+++ src/components/ContactModal.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect, useRef } from 'react';
-import emailjs from 'emailjs-com';
+import { useState, useEffect, useRef } from "react";
+import emailjs from "emailjs-com";
+import thankyoucheck from "../assets/thankyoucheck.png";
 
 function ContactModal({ closeModal }) {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   });
+  const [submitted, setSubmitted] = useState(false); // New state variable
 
   const modalRef = useRef(null);
 
@@ -21,17 +23,16 @@ function ContactModal({ closeModal }) {
     // Replace "YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", and "YOUR_USER_ID" with your actual values
     emailjs
       .sendForm(
-        'service_ge8matn',
-        'template_nh0bluk',
+        "service_ge8matn",
+        "template_nh0bluk",
         e.target,
-        'bD-Rm5YFO4MwTl9Py'
+        "bD-Rm5YFO4MwTl9Py"
       )
       .then(() => {
-        alert('Your message has been sent successfully!');
-        closeModal();
+        setSubmitted(true); // Update the state to indicate successful submission
       })
       .catch((error) => {
-        console.error('Error sending message:', error);
+        console.error("Error sending message:", error);
       });
   };
 
@@ -42,66 +43,92 @@ function ContactModal({ closeModal }) {
   };
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleOutsideClick);
+    document.addEventListener("mousedown", handleOutsideClick);
 
     return () => {
-      document.removeEventListener('mousedown', handleOutsideClick);
+      document.removeEventListener("mousedown", handleOutsideClick);
     };
   }, [handleOutsideClick]);
 
+  // Display the thank you message if the form is submitted
+  if (submitted) {
+    return (
+      <div className="fixed left-0 top-0 z-40 flex h-full w-full items-center justify-center bg-black bg-opacity-50 text-center">
+        <div
+          ref={modalRef}
+          className="relative mx-3 max-w-lg rounded-lg bg-white p-7 sm:mx-0"
+        >
+          <div className="mb-4 flex justify-center">
+            {/* Apply Tailwind CSS classes to adjust the size of the confirmation icon */}
+            <img
+              src={thankyoucheck}
+              alt="Confirmation Icon"
+              className="h-12 w-12"
+            />
+          </div>
+          <h1 className="mb-4 text-3xl font-bold">Thank You!</h1>
+          <p className="mb-10">
+            Thanks for reaching out. We will be in touch with you soon.
+          </p>
+          <button
+            type="button"
+            onClick={closeModal}
+            className="  rounded-sm bg-logoGreen px-6 py-2 font-bold text-white hover:bg-logoGreenHover"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  // Render the form if it's not submitted
   return (
-    <div className='fixed left-0 top-0 z-40 flex h-full w-full items-center justify-center bg-black bg-opacity-50'>
+    <div className="fixed left-0 top-0 z-40 flex h-full w-full items-center justify-center bg-black bg-opacity-50">
       <div
         ref={modalRef}
-        className='relative mx-3 max-w-md rounded-lg bg-white p-7 sm:mx-0'
+        className="relative mx-3 max-w-md rounded-lg bg-white p-7 sm:mx-0"
       >
-        {/* <button
-          onClick={closeModal}
-          className="absolute right-5 top-5 rounded-full bg-gray-300 p-1 text-white hover:text-gray-700"
-        >
-          <FaTimes size={12} />
-        </button> */}
-
-        <h1 className='mb-1 mt-2 text-3xl font-bold'>Get In Touch</h1>
-        <p className='mb-8'>We are here for you! How can we help?</p>
-        <form onSubmit={handleSubmit} className='space-y-5'>
+        <h1 className="mb-1 mt-2 text-3xl font-bold">Get In Touch</h1>
+        <p className="mb-8">We are here for you! How can we help?</p>
+        <form onSubmit={handleSubmit} className="space-y-5">
           <input
-            type='text'
-            name='name'
-            placeholder='Your Name'
+            type="text"
+            name="name"
+            placeholder="Your Name"
             value={formData.name}
             onChange={handleChange}
             required
-            className='w-full rounded-md border border-gray-300 p-2'
+            className="w-full rounded-md border border-gray-300 p-2"
           />
           <input
-            type='email'
-            name='email'
-            placeholder='Your Email'
+            type="email"
+            name="email"
+            placeholder="Your Email"
             value={formData.email}
             onChange={handleChange}
             required
-            className='w-full rounded-md border border-gray-300 p-2'
+            className="w-full rounded-md border border-gray-300 p-2"
           />
           <textarea
-            name='message'
-            placeholder='Your Message'
+            name="message"
+            placeholder="Your Message"
             value={formData.message}
             onChange={handleChange}
             required
-            className='h-32 w-full rounded-md border border-gray-300 p-2'
+            className="h-32 w-full rounded-md border border-gray-300 p-2"
           ></textarea>
-          <div className='flex justify-start'>
+          <div className="flex justify-start">
             <button
-              type='submit'
-              className='mr-4 rounded-sm bg-logoGreen px-6 py-2 font-bold text-white hover:bg-logoGreenHover'
+              type="submit"
+              className="mr-4 rounded-sm bg-logoGreen px-6 py-2 font-bold text-white hover:bg-logoGreenHover"
             >
               Send Message
             </button>
             <button
-              type='button'
+              type="button"
               onClick={closeModal}
-              className='mr-2 text-black'
+              className="mr-2 text-black"
             >
               Cancel
             </button>
