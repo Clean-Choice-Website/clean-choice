d41920f63ad1d831e65fbc2ce61a07029fccf54b
inathan44 <inathan44@yahoo.com>
2023-06-14 17:16:33 -0400

    merge branch

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,12 +1,9 @@
-const Review = () => {
+const Review = ({ children }) => {
   return (
     <div className='mx-auto flex max-w-5xl justify-center'>
       <div className='mx-4 max-w-[450px] rounded bg-white p-3 shadow-md md:mx-6 md:flex md:w-auto md:max-w-none md:flex-row-reverse md:items-center md:px-7 md:py-6'>
         <p className='mb-4 text-sm font-light text-gray-700 md:mb-0 md:w-2/3'>
-          Lorem ipsum dolor sit amet consect etur, adipisicing elit. Non,
-          deleniti consectetur nihil quaerat soluta explicabo enim Lorem ipsum
-          dolor sit amet consect etur, adipisicing elit. Non, deleniti
-          consectetur nihil quaerat soluta explicabo enim{' '}
+          {children}
         </p>
         <div className='flex justify-between md:w-1/3 md:flex-row-reverse md:items-center md:justify-end md:gap-4'>
           <div className=''>
