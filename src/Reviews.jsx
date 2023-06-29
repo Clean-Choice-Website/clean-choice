802709cc0ffcc7cefa39cc2e5094098d64347ac4
inathan44 <inathan44@yahoo.com>
2023-06-29 13:40:36 -0400

    review success modal

--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from 'react';
+/* eslint-disable react/prop-types */
+import { useState, useEffect, useRef } from 'react';
+import thankyoucheck from './assets/thankyoucheck.png';
+import { BarLoader } from 'react-spinners';
 // import emailjs from 'emailjs-com';
 import Review from './components/Review';
 import StarIcon from './components/StarIcon';
 import { scrollToTop } from './utils/scrollToTop';
-import { BarLoader } from 'react-spinners';
 
 const Reviews = () => {
   const [isSending, setIsSending] = useState(false);
+  // const [showModal, setShowModal] = useState(false);
+  const modalRef = useRef(null);
 
   const [formData, setFormData] = useState({
     name: '',
@@ -15,6 +19,15 @@ const Reviews = () => {
   });
 
   const [sentSuccessfully, setSentSuccessfully] = useState(false);
+
+  // const openModal = () => {
+  //   setShowModal(true);
+  // };
+
+  const closeModal = () => {
+    // setShowModal(false);
+    setSentSuccessfully(false);
+  };
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -52,9 +65,28 @@ const Reviews = () => {
     //   });
   };
 
+  // useEffect(() => {
+  //   if (sentSuccessfully) openModal();
+  // }, [sentSuccessfully, openModal]);
+
   useEffect(() => {
     scrollToTop();
   }, []);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleOutsideClick = (e) => {
+    if (modalRef.current && !modalRef.current.contains(e.target)) {
+      closeModal();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [handleOutsideClick]);
 
   return (
     <div className=' bg-slate-100 pb-8'>
@@ -70,14 +102,7 @@ const Reviews = () => {
           <StarIcon />
         </div>
       </div>
-      <div className='mx-4 flex flex-col gap-4'>
-        {/* {Array.from({ length: 10 }).map((_, idx) => (
-          <Testimonial key={idx}>
-            Lorem ipsum dolor sit amet consect etur, adipisicing elit. Non,
-            deleniti consectetur nihil quaerat soluta explicabo enim{' '}
-            </Testimonial>
-        ))} */}
-      </div>
+
       <div className='flex flex-col gap-6'>
         <Review name={'Brandon Neddef'}>
           {`Good service and friendly staff. Staff is very friendly, punctual and thorough!`}
@@ -154,6 +179,36 @@ const Reviews = () => {
           </div>
         </form>
       </div>
+
+      {/* Successful review sent modal */}
+      {sentSuccessfully && (
+        <div className='fixed left-0 top-0 z-40 flex h-full w-full items-center justify-center bg-black bg-opacity-50 text-center'>
+          <div
+            ref={modalRef}
+            className='relative mx-3 max-w-lg rounded-lg bg-white p-7 sm:mx-0'
+          >
+            <div className='mb-4 flex justify-center'>
+              {/* Apply Tailwind CSS classes to adjust the size of the confirmation icon */}
+              <img
+                src={thankyoucheck}
+                alt='Confirmation Icon'
+                className='h-12 w-12'
+              />
+            </div>
+            <h1 className='mb-4 text-3xl font-bold'>Thank You!</h1>
+            <p className='mb-10'>
+              Thanks for leaving a review. Your feedback helps us get better!
+            </p>
+            <button
+              type='button'
+              onClick={closeModal}
+              className='  rounded-sm bg-logoGreen px-6 py-2 font-bold text-white hover:bg-logoGreenHover'
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
