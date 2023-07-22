a7a87e895dc7407c5e98ce363010b063ee193255
Alex Bernt <alexbernt100@gmail.com>
2023-07-22 11:07:27 -0500

    Add loading screen

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { scrollToTop } from "./utils/scrollToTop";
+import { MoonLoader } from "react-spinners";
 import {
   commercialList,
   carpetList,
@@ -17,55 +18,88 @@ import ContactInfo from "./components/ContactInfo";
 import ServicesContent from "./components/ServicesContent";
 
 const Services = ({ openModal }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    scrollToTop();
+    const imagePromises = [
+      new Promise((resolve) => {
+        const img1 = new Image();
+        img1.src = CommercialCleaning;
+        img1.onload = resolve;
+      }),
+      new Promise((resolve) => {
+        const img2 = new Image();
+        img2.src = CleaningImg;
+        img2.onload = resolve;
+      }),
+      new Promise((resolve) => {
+        const img3 = new Image();
+        img3.src = FloorCare;
+        img3.onload = resolve;
+      }),
+    ];
+
+    Promise.all(imagePromises).then(() => {
+      setIsLoading(false);
+      scrollToTop();
+    });
   }, []);
 
   return (
     <div>
-      <ContactInfo />
-      <div className="mx-auto max-w-screen-sm lg:max-w-7xl">
-        <section className="mx-8 flex flex-col items-center justify-between py-4 lg:items-start">
-          <h1 className="mt-4 text-3xl font-bold">Our Services</h1>
-          <div className="w-full py-4 md:mx-0 lg:pb-10">
-            <p className="rounded-md bg-logoGreen px-4 py-4 text-center text-xl font-bold text-white lg:px-0">
-              Serving Waukesha and Milwaukee Counties for over 10 years
-            </p>
-          </div>
-          <div className="mx-8 flex flex-col gap-6 lg:mx-0">
-            <ServicesContent
-              image={CommercialCleaning}
-              name={"Commercial Cleaning"}
-              list={commercialList}
-              info={commercialInfo}
-              row={row}
-            />
-            <ServicesContent
-              image={CleaningImg}
-              name={"Carpet Cleaning"}
-              list={carpetList}
-              info={carpetInfo}
-              row={rowReverse}
-            />
-            <ServicesContent
-              image={FloorCare}
-              name={"Floor Care & Tile Re-waxing"}
-              list={floorList}
-              info={floorInfo}
-              row={row}
-            />
-          </div>
-        </section>
-        <div className="mb-4 flex flex-col items-center justify-center md:mx-8 md:mt-6 md:justify-start">
-          <p className="mb-6 font-bold">Have a Question regarding a service?</p>
-          <button
-            onClick={openModal}
-            className="inline-block w-32 rounded bg-logoGreen py-3 text-center text-xl font-bold text-white transition-all hover:bg-logoGreenHover"
-          >
-            Contact Us
-          </button>
+      {isLoading ? (
+        <div className="flex min-h-screen items-center justify-center">
+          <MoonLoader color={"#497429"} />
         </div>
-      </div>
+      ) : (
+        <div>
+          <ContactInfo />
+          <div className="mx-auto max-w-screen-sm lg:max-w-7xl">
+            <section className="mx-8 flex flex-col items-center justify-between py-4 lg:items-start">
+              <h1 className="mt-4 text-3xl font-bold">Our Services</h1>
+              <div className="w-full py-4 md:mx-0 lg:pb-10">
+                <p className="rounded-md bg-logoGreen px-4 py-4 text-center text-xl font-bold text-white lg:px-0">
+                  Serving Waukesha and Milwaukee Counties for over 10 years
+                </p>
+              </div>
+              <div className="mx-8 flex flex-col gap-6 lg:mx-0">
+                <ServicesContent
+                  image={CommercialCleaning}
+                  name={"Commercial Cleaning"}
+                  list={commercialList}
+                  info={commercialInfo}
+                  row={row}
+                />
+                <ServicesContent
+                  image={CleaningImg}
+                  name={"Carpet Cleaning"}
+                  list={carpetList}
+                  info={carpetInfo}
+                  row={rowReverse}
+                />
+                <ServicesContent
+                  image={FloorCare}
+                  name={"Floor Care & Tile Re-waxing"}
+                  list={floorList}
+                  info={floorInfo}
+                  row={row}
+                />
+              </div>
+            </section>
+            <div className="mb-4 flex flex-col items-center justify-center md:mx-8 md:mt-6 md:justify-start">
+              <p className="mb-6 font-bold">
+                Have a Question regarding a service?
+              </p>
+              <button
+                onClick={openModal}
+                className="inline-block w-32 rounded bg-logoGreen py-3 text-center text-xl font-bold text-white transition-all hover:bg-logoGreenHover"
+              >
+                Contact Us
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
