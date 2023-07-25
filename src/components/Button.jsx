572b6403501260582d2a2006b6f2d218784eeed0
Alex Bernt <alexbernt100@gmail.com>
2023-07-24 21:28:08 -0500

    remove console.log

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,6 @@
 import { Link } from "react-router-dom";
 
 const Button = ({ to, children }) => {
-  console.log("children", children);
-
   return (
     <Link to={`/${to}`}>
       <p className="w-52 rounded bg-gray-800 py-4 text-center text-2xl font-bold text-white transition-all hover:bg-ButtonBlackHover lg:inline-block">
