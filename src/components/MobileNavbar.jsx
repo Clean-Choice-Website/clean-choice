a075f0d19e58f9ac1f9e0e7007f53b67ac997f66
Alex Bernt <alexbernt100@gmail.com>
2023-07-24 21:28:40 -0500

    Add outside click event and hover effects

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -1,16 +1,44 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import phone from "../assets/phone.svg";
+import { useEffect, useRef } from "react";
 
 const MobileNavbar = ({ setShowMenu, openModal }) => {
+  const mobileNavbarRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      // Check if the clicked element has the "menu-icon" label
+      const isMenuIconClicked = e.target.dataset.label === "menu-icon";
+
+      // If the clicked element is the menu icon button, don't close the mobile navbar
+      if (
+        !isMenuIconClicked &&
+        mobileNavbarRef.current &&
+        !mobileNavbarRef.current.contains(e.target)
+      ) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [setShowMenu]);
+
   return (
     <div className="absolute z-20 h-[calc(100vh_-_100px)] w-full border bg-black bg-opacity-50 md:hidden">
-      <div className=" z-20 w-full border-t-2 border-gray-400 bg-slate-100">
+      <div
+        ref={mobileNavbarRef}
+        className="z-20 w-full border-t-2 border-gray-400 bg-slate-100"
+      >
         <ul className="mx-4 pb-2 text-xl">
           <Link to="/about">
             <li
               onClick={() => setShowMenu(false)}
-              className="border-b border-gray-300 py-4"
+              className="border-b border-gray-300 py-4 transition-all hover:text-logoGreen"
             >
               About Us
             </li>
@@ -19,7 +47,7 @@ const MobileNavbar = ({ setShowMenu, openModal }) => {
           <Link to="/services">
             <li
               onClick={() => setShowMenu(false)}
-              className="border-b border-gray-300 py-4"
+              className="border-b border-gray-300 py-4 transition-all hover:text-logoGreen"
             >
               Services
             </li>
@@ -28,7 +56,7 @@ const MobileNavbar = ({ setShowMenu, openModal }) => {
           <Link to="/Reviews">
             <li
               onClick={() => setShowMenu(false)}
-              className="border-b border-gray-300 py-4"
+              className="border-b border-gray-300 py-4 transition-all hover:text-logoGreen"
             >
               Reviews
             </li>
@@ -36,7 +64,7 @@ const MobileNavbar = ({ setShowMenu, openModal }) => {
           <Link to="/careers">
             <li
               onClick={() => setShowMenu(false)}
-              className="border-b border-gray-300 py-4"
+              className="border-b border-gray-300 py-4 transition-all hover:text-logoGreen"
             >
               Careers
             </li>
@@ -56,7 +84,12 @@ const MobileNavbar = ({ setShowMenu, openModal }) => {
           {/* </Link> */}
           <li className="flex justify-center gap-2 border-b border-gray-300 py-4 text-slate-500">
             <img src={phone} alt="Phone icon" className="w-6" />
-            <a href="tel:4143109206">(414)310-9206</a>
+            <a
+              href="tel:4143109206"
+              className="transition-all hover:text-logoGreen"
+            >
+              (414)310-9206
+            </a>
           </li>
         </ul>
       </div>
