2683ab49f57e8f4d3fd8b4bd30a1726b8021d362
Alex Bernt <alexbernt100@gmail.com>
2023-08-31 15:50:47 -0500

    Add follow us to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,4 @@
 import "../App.css";
-import twitter from "../assets/twitter-sign.png";
-import facebook from "../assets/facebook.png";
 import linkedin from "../assets/linkedin.png";
 import cleanChoice from "../assets/CleanChoice.png";
 import { Link } from "react-router-dom";
@@ -10,7 +8,7 @@ function Footer({ openModal }) {
     <>
       <footer className="mx-auto flex max-w-7xl flex-col items-center justify-between px-16 py-8 md:flex-row md:px-8">
         <Link to="/">
-          <div className="md: flex items-center gap-2 pb-4">
+          <div className="items-center gap-2 pb-4 md:flex">
             <img
               src={cleanChoice}
               alt="Clean Choice icon"
@@ -19,26 +17,42 @@ function Footer({ openModal }) {
           </div>
         </Link>
         <div>
-          <ul className="flex flex-col items-center gap-4 text-center md:flex-row md:items-start md:gap-6 md:text-left">
+          <ul className="flex flex-col items-center gap-4 text-center md:flex-row md:items-start md:text-left">
             <Link to="/about">
-              <li className="text-md w-20 font-bold transition-all hover:text-logoGreen md:w-20 lg:w-32 lg:text-lg">
+              <li className="text-md w-20 font-semibold transition-all hover:text-logoGreen md:w-20 lg:w-32 lg:text-lg">
                 About Us
               </li>
             </Link>
             <Link to="/services">
-              <li className="text-md w-20 font-bold transition-all hover:text-logoGreen md:w-20 lg:w-32 lg:text-lg">
+              <li className="text-md w-20 font-semibold transition-all hover:text-logoGreen md:w-20 lg:w-32 lg:text-lg">
                 Services
               </li>
             </Link>
             <Link to="/reviews">
-              <li className="text-md w-20 font-bold transition-all hover:text-logoGreen md:w-20 lg:w-32 lg:text-lg">
+              <li className="text-md w-20 font-semibold transition-all hover:text-logoGreen md:w-20 lg:w-32 lg:text-lg">
                 Reviews
               </li>
             </Link>
+            <div className="hidden md:block">
+              <li className="text-md hidden text-center font-semibold transition-all hover:cursor-pointer hover:text-logoGreen md:block md:w-20 md:text-left lg:w-32 lg:text-lg">
+                Follow Us
+              </li>
+              <ul className="hidden text-center text-xs md:block md:text-sm">
+                <a
+                  href="https://www.linkedin.com/company/cleanchoicellc"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <li className="ml-7">
+                    <img src={linkedin} alt="Linkedin icon" className="w-6" />
+                  </li>
+                </a>
+              </ul>
+            </div>
             <div>
               <li
                 onClick={openModal}
-                className="text-md text-center font-bold transition-all hover:cursor-pointer hover:text-logoGreen md:w-24 md:text-left lg:w-32 lg:text-lg"
+                className="text-md text-center font-semibold transition-all hover:cursor-pointer hover:text-logoGreen md:w-24 md:text-left lg:w-32 lg:text-lg"
               >
                 Contact Us
               </li>
@@ -62,25 +76,7 @@ function Footer({ openModal }) {
             <ul className="flex md:hidden">
               <li className="px-1">
                 <a
-                  href="https://twitter.com/CleanChoiceWI"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={twitter} alt="Twitter icon" className="w-6" />
-                </a>
-              </li>
-              <li className="px-1">
-                <a
-                  href="https://www.facebook.com/profile.php?id=100063456492630"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={facebook} alt="Facebook icon" className="w-6" />
-                </a>
-              </li>
-              <li className="px-1">
-                <a
-                  href="https://www.linkedin.com/in/michelle-roubik-lee/"
+                  href="https://www.linkedin.com/company/cleanchoicellc"
                   target="_blank"
                   rel="noreferrer"
                 >
