db9678792e6efc82027982a6119cdbc809319365
Alex Bernt <alexbernt100@gmail.com>
2023-09-14 12:16:35 -0500

    Create newsletter component

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,21 @@
+import Letter from "../assets/Newsletter.pdf";
+
+const Newsletter = () => {
+  return (
+    <>
+      <p className=" bg-logoGreen py-5 text-center font-semibold text-white sm:text-lg">
+        Check out our latest company newsletter{" "}
+        <a
+          href={Letter}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:text-[#c4da50]"
+        >
+          here
+        </a>
+      </p>
+    </>
+  );
+};
+
+export default Newsletter;
